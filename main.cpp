b532842553d740d06cf583a45672e0e2ca609b38
zhuch <zhuch@iauto.com>
2019-08-14 16:35:42 +0800

    add helloworld
    Change-Id: I5fbd1917c4ebe822df2b7419e0e98420ae62d4b1

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "stdio.h"
+int main(int argc, char** argv)
+{
+    printf("\n ********************************* \n");
+    printf("\n ********************************* \n");
+    printf("\n **        colazhu  test        ** \n");
+    printf("\n **         HELLO WORLD         ** \n");
+    printf("\n **        colazhu  test        ** \n");
+    printf("\n ********************************* \n");
+    printf("\n ********************************* \n");
+    return 0;
+}
